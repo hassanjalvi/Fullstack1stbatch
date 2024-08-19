837849bc0722de35d5eb921c9adf8b0819a7116b
hassanjalvi <hassanjalvi626@gmail.com>
2024-08-19 12:06:36 -0700

    Welcome to laravel tutorials

--- laravelclass1/resources/views/welcome.blade.php
+++ laravelclass1/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ecc</title>
+    <title>ECC</title>
 </head>
 <body>
     <h1>Welcome to Laravel Tutorials</h1>
