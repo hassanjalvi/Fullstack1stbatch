391826d9b54d8e28d1e15fc0085e1c696b893a73
hassanjalvi <hassanjalvi626@gmail.com>
2024-08-20 11:03:28 -0700

    pages show

--- laravelclass1/routes/web.php
+++ laravelclass1/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\CollectionController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\PrdocutController;
+use App\Http\Controllers\PrivacyController;
+use App\Http\Controllers\SignINController;
+use App\Http\Controllers\SignUpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+#Userside
+Route::get('/',[HomeController::class,'index'] );
+Route::get('/about',[AboutController::class,'index'] );
+Route::get('/collection',[CollectionController::class,'index'] );
+Route::get('/ingredient',[IngredientController::class,'index'] );
+Route::get('/product',[PrdocutController::class,'index'] );
+Route::get('/privacy',[PrivacyController::class,'index'] );
+Route::get('/signin',[SignINController::class,'index'] );
+Route::get('/signup',[SignUpController::class,'index'] );
